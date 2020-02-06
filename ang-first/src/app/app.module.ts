2d323b0c3e2100cb58a911e4b64af2bf95570533
makkiani <59272039+makkiani@users.noreply.github.com>
2020-02-05 21:29:11 -0600

    my angular changes

--- ang-first/src/app/app.module.ts
+++ ang-first/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './summary/summary.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule
---
+++ ang-first/src/app/summary/summary.component.spec.ts
@@ -0,0 +1,32 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SummaryComponent } from './summary.component';
+
+describe('SummaryComponent', () => {
+  let component: SummaryComponent;
+  let fixture: ComponentFixture<SummaryComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SummaryComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SummaryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+export class AppComponent {
+  team1 = "Ravens";
+  team = "Lagaan";
+  result = 1
+
+}
